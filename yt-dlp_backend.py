89289296aac782a53333251adc681316ed945d55
johnyyjack123 <jonathas.brandt@tutanota.de>
2025-08-15 18:42:17 +0200

    feat: Settings for video quality and container
    - Settings for video quality and container
    - Convert commands from yt-dlp to explaining text and back

--- yt-dlp_backend.py
+++ yt-dlp_backend.py
@@ -22,50 +22,67 @@ def home():
     # TODO: json Datei wird angelegt, wenn noch nicht vorhanden
     video_quality = ["best", "worst", "bestvideo", "bestaudio", "worstvideo", "worstaudio", "bestvideo+bestaudio/best"]
     video_resolution = ["720", "1080", "1920", "1440"]
-    with open("userdata.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-        download_folder = data["download_folder"]
+    video_container = ["mp4", "mov", "mkv", "webm", "avi"]
 
-        deafult_video_quality = data["video_quality"]
-        video_quality.remove(deafult_video_quality)
-        video_quality.insert(0, deafult_video_quality)
+    data = read("file")
+    download_folder = data["download_folder"]
 
-        deafult_video_resolution = data["video_resolution"]
-        video_resolution.remove(deafult_video_resolution)
-        video_resolution.insert(0, deafult_video_resolution)
-    return render_template('index.html', download_folder=download_folder, video_quality=video_quality, video_resolution=video_resolution)
+    deafult_video_quality = data["video_quality"]
+    video_quality.remove(deafult_video_quality)
+    video_quality.insert(0, deafult_video_quality)
+    for x, cmd in enumerate(video_quality):
+        description = convert_command_to_text(cmd)
+        video_quality[x] = description
+
+    deafult_video_resolution = data["video_resolution"]
+    video_resolution.remove(deafult_video_resolution)
+    video_resolution.insert(0, deafult_video_resolution)
+
+    deafult_video_container = data["video_container"]
+    video_container.remove(deafult_video_container)
+    video_container.insert(0, deafult_video_container)
+    return render_template('index.html',
+                           download_folder=download_folder,
+                           video_quality=video_quality,
+                           video_resolution=video_resolution,
+                           video_container=video_container)
 
 @app.route('/video_settings', methods=["GET", "POST"])
 def video_settings():
     custom_resolution = request.form.get("custom_resolution")
-    print("Checkbox: " + custom_resolution)
+    if custom_resolution:
+        print("Checkbox: " + custom_resolution)
     if custom_resolution == "yes":
         video_resolution = request.form.get("video_resolution")
         if not video_resolution:
             return "No resolution set"
-        video_resolution = 'bv[height<=' + video_resolution + ']+ba/best[height<=' + video_resolution + ']'
+        video_resolution_command = 'bv[height<=' + video_resolution + ']+ba/best[height<=' + video_resolution + ']'
         save("video_resolution", video_resolution)
-        video_data = video_resolution
+        save("video_resolution_command", video_resolution_command)
+        video_data = video_resolution_command
     else:
         video_quality = request.form.get("video_quality")
+        video_quality = convert_text_to_command(video_quality)
         save("video_quality", video_quality)
         video_data = video_quality
+    video_container = request.form.get("video_container")
+    save("video_container", video_container)
     video_url = request.form.get("video_url")
     print("URL: " + video_url)
-    start_download(video_url, video_data)
+    start_download(video_url, video_data, video_container)
     return redirect(url_for("home"))
 
 #@app.route('/download', methods=["GET", "POST"])
-def start_download(video_url, video_data):
+def start_download(video_url, video_data, video_container):
     global download_thread
     #video_url = request.form.get("yt-link")
     if not download_thread:
         download_thread = True
-        thread = threading.Thread(target=download, daemon=True, args=(video_url, video_data,))
+        thread = threading.Thread(target=download, daemon=True, args=(video_url, video_data, video_container,))
         thread.start()
     return
 
-def download(video_url, video_data):
+def download(video_url, video_data, video_container):
     global download_thread
     if video_url:
         print(f"Video-URL: {video_url}")
@@ -75,9 +92,6 @@ def download(video_url, video_data):
             'eta': 'N/A',
             'message': '⏳ Download wird vorbereitet...'
         })
-        #with open("userdata.json", "r", encoding="utf-8") as file:
-        #    data = json.load(file)
-        #    download_folder = data["download_folder"]
         download_folder = read("download_folder")
         if not os.path.exists(download_folder):
             return "Not valid folder"
@@ -87,6 +101,7 @@ def download(video_url, video_data):
         ydl_opts = {
             'format': video_data,  # schlechteste Qualität, um Beispiel zu zeigen
             'outtmpl': os.path.join(download_folder, '%(title)s.%(ext)s'),
+            'merge_output_format': video_container,
             'progress_hooks': [progress_hook],
         }
         print(ydl_opts)
@@ -130,9 +145,6 @@ def search_download_folder(folder, path):
 def change_download_folder():
     with open("userdata.json", "r", encoding="utf-8") as file:
         data = json.load(file)
-        #path = data["download_folder"]
-        #if not path or not os.path.exists(path):
-        #    download_folder = os.path.expanduser("~") + "\\" + "Videos"
         data["download_folder"] = request.args.get("path")
     with open("userdata.json", "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
@@ -178,10 +190,43 @@ def save(entry, video_data):
     return
 
 def read(entry):
-    with open("userdata.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-        video_data = data[entry]
-        return video_data
+    if entry == "file":
+        with open("userdata.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+            return data
+    elif entry:
+        with open("userdata.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+            video_data = data[entry]
+            return video_data
+
+def convert_command_to_text(cmd):
+    video_quality_cmd = ["best", "worst", "bestvideo", "bestaudio", "worstvideo", "worstaudio", "bestvideo+bestaudio/best"]
+
+    video_quality = ["Best video and audio",
+                     "Worst video and audio",
+                     "Best video (video only)",
+                     "Best audio (audio only)",
+                     "Worst video (video only)",
+                     "Worst audio (audio only",
+                     "Best video and best audio (seperated downloads, merged after download, provides in some cases better quality)"]
+    index = video_quality_cmd.index(cmd)
+    description = video_quality[index]
+    return description
+
+def convert_text_to_command(description):
+    video_quality_cmd = ["best", "worst", "bestvideo", "bestaudio", "worstvideo", "worstaudio",
+                         "bestvideo+bestaudio/best"]
+
+    video_quality = ["Best video and audio",
+                     "Worst video and audio",
+                     "Best video (video only)",
+                     "Best audio (audio only)",
+                     "Worst video (video only)",
+                     "Best video and best audio (seperated downloads, merged after download, provides in some cases better quality)"]
+    index = video_quality.index(description)
+    cmd = video_quality_cmd[index]
+    return cmd
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
